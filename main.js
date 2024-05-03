fe0853a2e083d072f63f8ca165392325cd1c3c2c
SURU EMMANUEL <suruaino@gmail.com>
2024-05-03 11:28:45 +0100

    added some ui styles

--- main.js
+++ main.js
@@ -1,7 +1,10 @@
 
 const dialog = document.getElementById('myDialog');
-const 
+const mainContentBx = document.querySelector(".contentbx");
+const mainImgBx = document.querySelector(".imgbx");
 function openDialog(event){
+    mainContentBx.style.display = "none";
+    mainImgBx.style.display = "none";
     dialog.style.display ="flex";
     dialog.style.flexDirection ="column";
     dialog.style.gap = 1 + "rem";
@@ -11,6 +14,8 @@ function openDialog(event){
 }
 
 function closeDialog(){
+    mainContentBx.style.display = "flex";
+    mainImgBx.style.display = "flex";
     dialog.close();
     dialog.style.display ="none";
 }
